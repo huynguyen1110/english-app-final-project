8b24909d37108310ba1ec51f3e82677dd434e9c0
huy nguyen <huynn@hivetech.vn>
2024-05-22 16:07:45 +0700

    create carosel component

---
+++ learning-english-app-ui/components/carousel/data.ts
@@ -0,0 +1,16 @@
+const data = [
+    {
+        title: "200 colocations for ielts 8.0 ",
+        imgUrl: "https://picsum.photos/id/11/200/300",
+    },
+    {
+        title: "In turpis",
+        imgUrl: "https://picsum.photos/id/10/200/300",
+    },
+    {
+        title: "Lorem Ipsum",
+        imgUrl: "https://picsum.photos/id/12/200/300",
+    },
+];
+
+export default data;
--- learning-english-app-ui/screens/home/HomeScreen.tsx
+++ learning-english-app-ui/screens/home/HomeScreen.tsx
@@ -1,10 +1,15 @@
-import {SafeAreaView, StatusBar} from "react-native";
+import {
+    SafeAreaView,
+    StatusBar,
+    ScrollView
+} from "react-native";
 import {GlobalStyles} from "../../styles/GlobalStyles";
 import {
-    Block,
-    Text,
+    Block, Input,
+    Text, theme,
 } from "galio-framework";
 import HeaderComponent from "../../components/HeaderComponent";
+import CarouselCards from "../../components/carousel/CarouselCards";
 
 const HomeScreen = () => {
     return (
@@ -14,10 +19,48 @@ const HomeScreen = () => {
             ]
         }>
             <StatusBar hidden/>
-            <HeaderComponent/>
-            <Block>
-                <Text>Home screen</Text>
-            </Block>
+            <ScrollView>
+
+                <HeaderComponent/>
+
+                <Block height={12}></Block>
+
+                <Block style={GlobalStyles.main_container}>
+                    <Block>
+                        <Block>
+                            <Text bold size={20}>Dictionary</Text>
+                        </Block>
+                        <Block>
+                            <Input rounded
+                                   icon="search"
+                                   family="Feather"
+                                   borderless={true}
+                                   bgColor={theme.COLORS?.NAVBAR}
+                                   placeholder="Search"></Input>
+                        </Block>
+                        <Block height={12}></Block>
+                        <Block row>
+                            <Text> <Input placeholder="Hello" rounded editable={false}></Input> </Text>
+                            <Text> <Input placeholder="Information" rounded editable={false}></Input> </Text>
+                        </Block>
+                    </Block>
+                </Block>
+
+                <Block height={12}></Block>
+                <Block style={[GlobalStyles.gray_background]} height={12}></Block>
+
+                <Block>
+                    <Block height={4}></Block>
+                    <Block style={[GlobalStyles.main_container]}>
+                        <Text bold size={20}>Resources</Text>
+                    </Block>
+                    <Block height={4}></Block>
+                    <Block center>
+                        <CarouselCards/>
+                    </Block>
+                </Block>
+
+            </ScrollView>
         </SafeAreaView>
     );
 }
