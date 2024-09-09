d238c1efc6f3b341fbcb93be89aa921be5aae181
huy nguyen <huynn@hivetech.vn>
2024-09-09 16:56:03 +0700

    create update user api

--- Api/src/main/java/com/example/api/dtos/authentication/RegisterDto.java
+++ Api/src/main/java/com/example/api/dtos/authentication/RegisterDto.java
@@ -1,6 +1,9 @@
 package com.example.api.dtos.authentication;
 
+import com.example.api.entities.enums.UserRole;
 import lombok.*;
+
+import java.util.Set;
 
 @AllArgsConstructor
 @Getter
@@ -15,4 +18,10 @@ public class RegisterDto {
     private String email;
 
     private String phoneNumber;
+
+    private String address;
+
+    private String status;
+
+    private Set<UserRole> roles;
 }
--- Api/src/main/java/com/example/api/entities/enums/UserRole.java
+++ Api/src/main/java/com/example/api/entities/enums/UserRole.java
@@ -2,5 +2,6 @@ package com.example.api.entities.enums;
 
 public enum UserRole {
     USER,
-    ADMIN
+    ADMIN,
+    SUPER_ADMIN
 }
