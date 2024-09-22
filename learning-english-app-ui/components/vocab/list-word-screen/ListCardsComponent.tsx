1fc242047504be52cc1f1162d434c4f2bf072c1e
huy nguyen <huynn@hivetech.vn>
2024-09-22 15:09:37 +0700

    create list card screen

--- learning-english-app-ui/components/vocab/list-word-screen/ListCardsComponent.tsx
+++ learning-english-app-ui/components/vocab/list-word-screen/ListCardsComponent.tsx
@@ -1,9 +1,11 @@
-import {StyleSheet, TouchableOpacity, View} from "react-native";
+import {Image, StyleSheet, TouchableOpacity, View} from "react-native";
 import {Block, Text, theme} from "galio-framework";
 import React from "react";
 import {IndexPath, Select, SelectItem} from "@ui-kitten/components";
 // @ts-ignore
 import Entypo from 'react-native-vector-icons/Entypo';
+// @ts-ignore
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import {GlobalStyles} from "../../../styles/GlobalStyles";
 
 const ListCardsComponent = (wordsData: any) => {
@@ -57,6 +59,35 @@ const ListCardsComponent = (wordsData: any) => {
                         </Block>
                         <Block height={4}></Block>
                         <Text size={16}>verb</Text>
+                        <Block height={4}></Block>
+
+                        <Block height={30} row alignItems="center">
+                            <Block>
+                                <TouchableOpacity style={{ padding: 4 }}>
+                                    <Text size={18} style={{ opacity: 0.5 }}><FontAwesome size={20} name="volume-up"/></Text>
+                                </TouchableOpacity>
+                            </Block>
+                            <Block width={12}></Block>
+                            <Block height={30}>
+                                <Text style={{ opacity: 0.5 }} size={18}>
+                                    phonetic
+                                </Text>
+                            </Block>
+                        </Block>
+
+                        <Block height={6}></Block>
+
+                        <Block>
+                            <Image style={styles.exampleImg} source={ require("../../../assets/imgs/background-wellcome-image-background.jpeg") } />
+                        </Block>
+
+                        <Block height={8}></Block>
+
+                        <Block>
+                            <Text size={16}>Meaning of the word.</Text>
+                        </Block>
+
+                        <Block height={12}></Block>
 
                     </Block>
                 </View>
@@ -75,7 +106,7 @@ const styles = StyleSheet.create({
     cardContainer: {
         backgroundColor: 'white',
         width: "95%",
-        height: 150,
+        minHeight: 300,
         marginLeft: "auto",
         marginRight: "auto",
         // Đổ bóng cho Android
@@ -91,5 +122,11 @@ const styles = StyleSheet.create({
 
         // Bo góc nếu cần
         borderRadius: 10,
+    },
+    exampleImg: {
+        width: 250,
+        height: 150,
+        resizeMode: "stretch",
+        borderRadius: 10
     }
 });
