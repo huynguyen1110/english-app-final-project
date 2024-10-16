a100dbe92444dcd03dc32fdb3474d07215e44241
huy nguyen <huynn@hivetech.vn>
2024-10-16 17:17:06 +0700

    create true false game ui

--- learning-english-app-ui/screens/vocab/TestScreen.tsx
+++ learning-english-app-ui/screens/vocab/TestScreen.tsx
@@ -1,9 +1,12 @@
-import {SafeAreaView, TouchableOpacity, View} from "react-native";
+import {SafeAreaView, StyleSheet, TouchableOpacity, View, ProgressBarAndroid, ScrollView, Image} from "react-native";
 import React from 'react'
 import {GlobalStyles} from "../../styles/GlobalStyles";
 import {useNavigation, useRoute} from "@react-navigation/native";
 // @ts-ignore
 import Feather from 'react-native-vector-icons/Feather';
+import {Text} from "galio-framework";
+import {ProgressBar, MD3Colors} from 'react-native-paper';
+import * as Progress from 'react-native-progress';
 
 const TestScreen = () => {
 
@@ -15,11 +18,45 @@ const TestScreen = () => {
 
     const [wordsData, setWordsData] = React.useState<any []>([]);
 
-    const [trueFlaseQuestions, setTrueFlaseQuestions] = React.useState<any []>([]);
+    // true false question data
+    const [trueFalseQuestions, setTrueFalseQuestions] = React.useState<any []>([]);
+
+    const [currentQuestionIndex, setCurrentQuestionIndex] = React.useState(0); // Chỉ số câu hỏi hiện tại
+
+    const [answered, setAnswered] = React.useState(false); // Kiểm soát xem đã trả lời chưa
+
+    const [currentQuestion, setCurrentQuestion] = React.useState<any>(null);
+
+    const [numberOfCorrectQuestions, setNumberOfCorrectQuestions] = React.useState<number>(0);
+
+    const [percentOftrueQuestions, setPercentOftrueQuestions] = React.useState<any>(0);
+
+    const [isGameFinished, setIsGameFinished] = React.useState(false);
 
     const goBackBtn = () => {
         navigation.goBack();
     }
+
+    const handleTrueFalseAnswer = (isCorrect: boolean) => {
+        setAnswered(true);
+        if (isCorrect === currentQuestion?.isCorrect) {
+            console.log("Correct!");
+            setNumberOfCorrectQuestions(numberOfCorrectQuestions + 1);
+        } else {
+            console.log("Incorrect!");
+        }
+    };
+
+    const handleNextQuestion = () => {
+        setAnswered(false); // Đặt lại trạng thái đã trả lời
+        if (currentQuestionIndex < trueFalseQuestions.length - 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1); // Chuyển sang câu hỏi tiếp theo
+        } else {
+            setIsGameFinished(true);
+            setPercentOftrueQuestions(numberOfCorrectQuestions / trueFalseQuestions?.length);
+            console.log("You've completed the game!");
+        }
+    };
 
     // Function to get a random incorrect meaning (from the same data list)
     const getRandomIncorrectMeaning = (list: any, correctMeaning: any) => {
@@ -55,30 +92,166 @@ const TestScreen = () => {
 
 
     React.useEffect(() => {
+        // take random question
         const wordsDatRandom = dataParams?.wordsData?.sort(() => 0.5 - Math.random()).slice(0, dataParams?.numberOfQuestions);
         setWordsData(wordsDatRandom)
     }, [dataParams])
 
     React.useEffect(() => {
-        const trueFalseQuestions = createTrueFalseQuestion(wordsData);
-        setTrueFlaseQuestions(trueFalseQuestions);
+        if (dataParams?.isTrueFalseOption) {
+            if (trueFalseQuestions?.length == 0) {
+                // create true false question base on random question above
+                const trueFalseQuestions = createTrueFalseQuestion(wordsData);
+                setTrueFalseQuestions(trueFalseQuestions);
+            }
+        }
     }, [wordsData]);
 
+    // Khi trueFalseQuestions thay đổi, set câu hỏi hiện tại
     React.useEffect(() => {
-        console.log(trueFlaseQuestions)
-    }, [trueFlaseQuestions]);
+        if (trueFalseQuestions.length > 0) {
+            setCurrentQuestion(trueFalseQuestions[currentQuestionIndex]);
+        }
+    }, [trueFalseQuestions, currentQuestionIndex]);
+
+    // Sử dụng useEffect để chuyển sang câu hỏi tiếp theo
+    React.useEffect(() => {
+        if (answered) {
+            handleNextQuestion();
+        }
+    }, [answered]);
 
     return (
         <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
             <View style={GlobalStyles.main_container}>
-                <View style={[{flexDirection: 'row'}]}>
+                <View
+                    style={[GlobalStyles.flex_row, GlobalStyles.align_item_center, GlobalStyles.justify_content_space_between]}>
                     <TouchableOpacity style={[{padding: 6}]} onPress={goBackBtn}>
                         <Feather size={35} name="x"/>
                     </TouchableOpacity>
+                    {
+                        dataParams?.isTrueFalseOption ? (
+                            <View>
+                                <Text size={28}>{currentQuestionIndex + 1}/{trueFalseQuestions?.length}</Text>
+                            </View>
+                        ) : (
+                            <View>
+                            </View>
+                        )
+                    }
+                    <View style={{width: 35}}></View>
                 </View>
             </View>
+            {
+                dataParams?.isTrueFalseOption ? (
+                    <ProgressBar
+                        progress={(currentQuestionIndex + 1) / (trueFalseQuestions?.length || 1)}
+                        color={MD3Colors.error50}
+                    />
+                ) : (
+                    <View></View>
+                )
+            }
+            {
+                !isGameFinished ? (
+                    dataParams?.isTrueFalseOption ? (
+                        <View style={{padding: 20}}>
+                            <View>
+                                <Text bold size={18}>Word:</Text>
+                                <Text size={26}>{currentQuestion?.word}</Text>
+                                <View style={{height: 4}}></View>
+                                {currentQuestion?.showCorrectMeaning === false ? (
+                                    <View>
+                                        <Text bold size={18}>Meaning:</Text>
+                                        <Text size={26}>{currentQuestion?.incorrectMeaning}</Text>
+                                    </View>
+                                ) : (
+                                    <View>
+                                        <Text bold size={18}>Meaning:</Text>
+                                        <Text size={26}>{currentQuestion?.meaning}</Text>
+                                    </View>
+                                )}
+                            </View>
+                            {answered ? (
+                                <View>
+                                    <Text>Answer: {currentQuestion.isCorrect ? "True" : "False"}</Text>
+                                </View>
+                            ) : (
+                                <View>
+                                    <View style={{height: 14}}></View>
+                                    <Text size={16}>Choose the correct answer:</Text>
+                                    <View style={{height: 12}}></View>
+                                    <TouchableOpacity style={styles.trueFlaseBtn}
+                                                      onPress={() => handleTrueFalseAnswer(true)}>
+                                        <Text size={18}>True</Text>
+                                    </TouchableOpacity>
+                                    <View style={{height: 20}}></View>
+                                    <TouchableOpacity style={styles.trueFlaseBtn}
+                                                      onPress={() => handleTrueFalseAnswer(false)}>
+                                        <Text size={18}>False</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            )}
+                        </View>
+                    ) : (
+                        <View></View>
+                    )
+                ) : (
+                    // Hiển thị kết quả khi trò chơi kết thúc
+                    <ScrollView style={GlobalStyles.main_container}>
+                        <View style={{height: 36}}></View>
+                        <View
+                            style={[GlobalStyles.flex_row, GlobalStyles.align_item_center, GlobalStyles.justify_content_space_between]}>
+                            <View style={{width: "75%"}}>
+                                <Text bold size={25}>You are learning something!</Text>
+                                <Text size={20}>Practice your incorrectly answered terms with the learning mode until
+                                    you get them all right.</Text>
+                            </View>
+                            <Image style={styles.winIcon} source={require('../../assets/icon-png/achievement.png')}/>
+                        </View>
+
+                        <View style={{height: 26}}></View>
+
+                        <View>
+                            <Text bold size={18}>Your result</Text>
+                            <View style={[GlobalStyles.flex_collum, GlobalStyles.align_item_center]}>
+                                <Progress.Circle
+                                    progress={percentOftrueQuestions}
+                                    size={100}
+                                    color="green"
+                                    unfilledColor={MD3Colors.error50}
+                                ></Progress.Circle>
+                                <View style={{height: 6}}></View>
+                                <Text>{percentOftrueQuestions} %</Text>
+                            </View>
+                        </View>
+                        <Text>You answered {numberOfCorrectQuestions} out of {trueFalseQuestions.length} questions
+                            correctly.</Text>
+
+                    </ScrollView>
+                )
+            }
+
+
         </SafeAreaView>
     );
 }
 
 export default TestScreen
+
+const styles = StyleSheet.create({
+    trueFlaseBtn: {
+        width: "100%",
+        height: 50,
+        borderWidth: 2,
+        borderColor: "gray",
+        borderRadius: 4,
+        justifyContent: "center",
+        padding: 6
+    },
+    winIcon: {
+        width: "20%",
+        height: 100,
+        resizeMode: 'stretch'
+    }
+});
--- learning-english-app-ui/utils/API.ts
+++ learning-english-app-ui/utils/API.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://192.168.94.173:9000';
+export const BASE_URL = 'http://192.168.1.173:9000';
 
 // authentication
 export const REGISTER_URI = '/api/v1/user/register';
