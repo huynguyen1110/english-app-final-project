ea69b9f2f0aeafe5efbd08a3ffacc3b005c8685b
huy nguyen <huynn@hivetech.vn>
2024-10-30 16:41:12 +0700

    fix news detail ui

--- vuejs-ui/src/pages/news-management/NewsDetail.vue
+++ vuejs-ui/src/pages/news-management/NewsDetail.vue
@@ -3,6 +3,14 @@ import { onMounted, ref } from 'vue';
 import { useToast } from 'primevue/usetoast';
 import { createNewsService, getArticleContent } from '@/service/news/NewsService';
 import { CATEGORY, SOURCE_NEWS_NAME } from '@/utils/Constaints';
+import { EditorContent, useEditor } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+import Text from '@tiptap/extension-text';
+import Heading from '@tiptap/extension-heading';
+import Document from '@tiptap/extension-document';
+import Paragraph from '@tiptap/extension-paragraph';
+import TextAlign from '@tiptap/extension-text-align';
+import Highlight from '@tiptap/extension-highlight';
 
 const toast = useToast();
 
@@ -16,6 +24,27 @@ let content = ref(null);
 let url = ref('');
 let author = ref('');
 let publishedAt = ref('');
+
+const editor = useEditor({
+    content: content.value,
+    extensions: [
+        StarterKit,
+        Text,
+        Heading.configure({
+            levels: [1, 2, 3]
+        }),
+        Document,
+        Paragraph,
+        TextAlign.configure({
+            types: ['heading', 'paragraph']
+        }),
+        Highlight],
+    editorProps: {
+        attributes: {
+            class: 'prose max-w-none [&_ol]:list-decimal [&_ul]:list-disc'
+        }
+    }
+});
 
 const dropdownSourceNewsValue = ref(null);
 const dropdownSourceNewsValues = ref([
@@ -46,6 +75,7 @@ onMounted(() => {
     url.value = newsData.value?.url;
     author.value = newsData.value?.author;
     publishedAt.value = newsData.value?.publishedAt;
+    content.value = newsData.value?.content;
     fetchGetArticleContent(url.value);
 });
 
@@ -130,15 +160,80 @@ const saveBtn = async () => {
         <div :style="{height: '30px'}"></div>
         <h6 class="title">{{ description }}</h6>
         <div :style="{height: '30px'}"></div>
-        <Button @click="isEditing = !isEditing">
+        <Button @click="() => {
+            isEditing = !isEditing;
+            console.log(isEditing)
+            if (isEditing) {
+                editor.commands.setContent(content);
+                return;
+            }
+            content = editor.getHTML();
+        }">
             {{ isEditing ? 'Save' : 'Edit' }}
         </Button>
         <!-- Hiển thị nội dung (khi không chỉnh sửa) -->
-        <div v-if="!isEditing" v-html="content" class="content"></div>
+        <div v-if="!isEditing" class="content">
+            <div v-html="content" class="prose"></div>
+        </div>
         <!-- Chế độ chỉnh sửa (khi đang chỉnh sửa) -->
         <div v-else>
-            <textarea v-model="content" class="edit-content"></textarea>
+            <div v-if="editor" class="content-container mt-6">
+                <div class="control-group">
+                    <div class="button-group">
+                        <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+                                :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+                            H1
+                        </button>
+                        <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+                                :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+                            H2
+                        </button>
+                        <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+                                :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
+                            H3
+                        </button>
+                        <button @click="editor.chain().focus().setParagraph().run()"
+                                :class="{ 'is-active': editor.isActive('paragraph') }">
+                            Paragraph
+                        </button>
+                        <button @click="editor.chain().focus().toggleBold().run()"
+                                :class="{ 'is-active': editor.isActive('bold') }">
+                            Bold
+                        </button>
+                        <button @click="editor.chain().focus().toggleItalic().run()"
+                                :class="{ 'is-active': editor.isActive('italic') }">
+                            Italic
+                        </button>
+                        <button @click="editor.chain().focus().toggleStrike().run()"
+                                :class="{ 'is-active': editor.isActive('strike') }">
+                            Strike
+                        </button>
+                        <button @click="editor.chain().focus().toggleHighlight().run()"
+                                :class="{ 'is-active': editor.isActive('highlight') }">
+                            Highlight
+                        </button>
+                        <button @click="editor.chain().focus().setTextAlign('left').run()"
+                                :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }">
+                            Left
+                        </button>
+                        <button @click="editor.chain().focus().setTextAlign('center').run()"
+                                :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }">
+                            Center
+                        </button>
+                        <button @click="editor.chain().focus().setTextAlign('right').run()"
+                                :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }">
+                            Right
+                        </button>
+                        <button @click="editor.chain().focus().setTextAlign('justify').run()"
+                                :class="{ 'is-active': editor.isActive({ textAlign: 'justify' }) }">
+                            Justify
+                        </button>
+                    </div>
+                </div>
+                <editor-content class="content-editor-style mt-2" :editor="editor" />
+            </div>
         </div>
+
         <p class="content"
            :style="{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', display: 'flex'}">
             <a :href="url" target="_blank">Click here to read more</a>
@@ -188,5 +283,135 @@ const saveBtn = async () => {
     width: 100%;
     min-height: 400px;
     box-sizing: border-box; /* Đảm bảo padding không làm thay đổi kích thước */
+}
+
+/* Basic editor styles */
+:first-child {
+    margin-top: 0;
+}
+
+/* List styles */
+ul,
+ol {
+    padding: 0 1rem;
+    margin: 1.25rem 1rem 1.25rem 0.4rem;
+
+    li p {
+        margin-top: 0.25em;
+        margin-bottom: 0.25em;
+    }
+}
+
+/* Heading styles */
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    line-height: 1.1;
+    margin-top: 2.5rem;
+    //text-wrap: pretty;
+    white-space: pre-line;
+}
+
+h1,
+h2 {
+    margin-top: 3.5rem;
+    margin-bottom: 1.5rem;
+}
+
+h1 {
+    font-size: 1.4rem;
+}
+
+h2 {
+    font-size: 1.2rem;
+}
+
+h3 {
+    font-size: 1.1rem;
+}
+
+h4,
+h5,
+h6 {
+    font-size: 1rem;
+}
+
+/* Code and preformatted text styles */
+code {
+    background-color: var(--purple-light);
+    border-radius: 0.4rem;
+    color: var(--black);
+    font-size: 0.85rem;
+    padding: 0.25em 0.3em;
+}
+
+pre {
+    background: var(--black);
+    border-radius: 0.5rem;
+    color: var(--white);
+    font-family: 'JetBrainsMono', monospace;
+    margin: 1.5rem 0;
+    padding: 0.75rem 1rem;
+
+    code {
+        background: none;
+        color: inherit;
+        font-size: 0.8rem;
+        padding: 0;
+    }
+}
+
+mark {
+    background-color: #FAF594;
+    border-radius: 0.4rem;
+    box-decoration-break: clone;
+    padding: 0.1rem 0.3rem;
+}
+
+blockquote {
+    border-left: 3px solid var(--gray-3);
+    margin: 1.5rem 0;
+    padding-left: 1rem;
+}
+
+hr {
+    border: none;
+    border-top: 1px solid var(--gray-2);
+    margin: 2rem 0;
+}
+
+.button-group {
+    display: flex; /* Sắp xếp các nút theo hàng */
+    gap: 10px; /* Khoảng cách giữa các nút */
+    background-color: #f0f0f0; /* Màu nền cho nhóm nút */
+    border-radius: 8px; /* Đường viền tròn cho nhóm */
+    padding: 10px; /* Khoảng cách bên trong cho nhóm */
+}
+
+.button-group button {
+    background-color: #ffffff; /* Màu nền mặc định cho các nút */
+    border: none; /* Không có viền */
+    border-radius: 4px; /* Đường viền tròn cho các nút */
+    padding: 10px 15px; /* Khoảng cách bên trong cho các nút */
+    cursor: pointer; /* Thay đổi con trỏ khi di chuột vào nút */
+    transition: background-color 0.3s ease, color 0.3s ease; /* Hiệu ứng chuyển màu mượt mà */
+}
+
+.button-group button:hover {
+    background-color: #d1c4e9; /* Màu nền khi hover */
+}
+
+.button-group button.is-active {
+    background-color: #9b59b6; /* Màu nền tím khi nút đang hoạt động */
+    color: white; /* Màu chữ trắng khi nút đang hoạt động */
+}
+
+.content-editor-style {
+    border-width: 2px;
+    border-radius: 10px;
+    min-height: 250px;
 }
 </style>
